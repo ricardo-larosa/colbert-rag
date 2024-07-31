fda45dd54dd339a4a03249ddac2c4577bdb40b9c
Ricardo La Rosa <ricardo@larosa.dev>
2024-07-31 22:21:28 +0000

    load existing index

--- src/server.py
+++ src/server.py
@@ -1,35 +1,38 @@
 import grpc
+import argparse
 from concurrent import futures
 from colbertrag_pb2 import Response, Document
 from colbertrag_pb2_grpc import add_ColbertRAGServicer_to_server, ColbertRAGServicer
 from ragatouille import RAGPretrainedModel
 from wikipedia import get_wikipedia_page
 
-RAG = RAGPretrainedModel.from_pretrained("colbert-ir/colbertv2.0")
-pages = ["2024_United_States_presidential_election", "2024_United_States_elections"]
-collection = [get_wikipedia_page(page) for page in pages]
-RAG.index(
-    collection=collection,
-    index_name="US_Elections-2024",
-    max_document_length=180,
-    split_documents=True,
-)
+RAGATOUILLE_PATH = "../.ragatouille/colbert/indexes"
 
 class ColbertRAGServicer(ColbertRAGServicer):
+    def __init__(self, model):
+        self.RAG = model
+
     def Retrieve(self, request, context):
         k = request.k if request.k > 0 else 1
-        retriever = RAG.as_langchain_retriever(k=k)
-        documents = [Document(page_content=doc.page_content, metadata=doc.metadata) 
+        retriever = self.RAG.as_langchain_retriever(k=k)
+        documents = [Document(page_content=doc.page_content, metadata=doc.metadata)
                      for doc in retriever.invoke(request.query)]
-
         return Response(documents=documents)
 
-def serve():
+def serve(model, port):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    add_ColbertRAGServicer_to_server(ColbertRAGServicer(), server)
-    server.add_insecure_port('[::]:50051')
+    add_ColbertRAGServicer_to_server(ColbertRAGServicer(model), server)
+    server.add_insecure_port(f'[::]:{port}')
     server.start()
     server.wait_for_termination()
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="ColbertRAG server")
+    parser.add_argument("--index_name", type=str, help="Path to the ColbertRAG index")
+    parser.add_argument("--port", type=int, default=50051, help="Port to run the server on (default: 50051)")
+    return parser.parse_args()
+
 if __name__ == '__main__':
-    serve()
+    args = parse_arguments()
+    RAG = RAGPretrainedModel.from_index(f'{RAGATOUILLE_PATH}/{args.index_name}')
+    serve(RAG, args.port)
