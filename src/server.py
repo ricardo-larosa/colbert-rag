0b1d32cba1d4b5cfe90c0a9f5a7661d762752319
ricardo-larosa <ricardo@larosa.dev>
2024-08-01 17:52:58 +0200

    Adding FastAPI support

--- src/server.py
+++ src/server.py
@@ -1,44 +1,33 @@
-import grpc
+import grpc_server
 import argparse
-from concurrent import futures
-from colbertrag_pb2 import Response, Document
-from colbertrag_pb2_grpc import add_ColbertRAGServicer_to_server, ColbertRAGServicer
+from fastapi import FastAPI
 from ragatouille import RAGPretrainedModel
+import grpc_server
+import fastapi_server
+import logging
 
 RAGATOUILLE_PATH = "../.ragatouille/colbert/indexes"
 
-class ColbertRAGServicer(ColbertRAGServicer):
-    def __init__(self, model):
-        self.RAG = model
-
-    def Retrieve(self, request, context):
-        """Get documents relevant to a query."""
-        k = request.k if request.k > 0 else 1
-        documents = [
-            Document(
-                page_content=doc["content"], metadata=doc.get("document_metadata", {})
-            )
-            for doc in self.RAG.search(query=request.query, k=k)
-        ]
-        
-        return Response(documents=documents)
-
-def serve(model, port, max_workers):
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=max_workers))
-    add_ColbertRAGServicer_to_server(ColbertRAGServicer(model), server)
-    server.add_insecure_port(f'[::]:{port}')
-    server.start()
-    server.wait_for_termination()
-
 def parse_arguments():
     parser = argparse.ArgumentParser(description="ColbertRAG server")
-    parser.add_argument("--index_name", type=str, help="Path to the ColbertRAG index")
-    parser.add_argument("--port", type=int, default=50051, help="Port to run the server on (default: 50051)")
+    parser.add_argument("--server", choices=['grpc', 'fastapi'], default='grpc', help="Server type (default: grpc)")
+    parser.add_argument("--host", type=str, help="Host to bind the server to")
+    parser.add_argument("--index", type=str, help="Path to the ColbertRAG index")
+    parser.add_argument("--port", type=int, help="Port to run the server on (default: 50051)")
     parser.add_argument("--max_workers", type=int, default=10, help="Maximum number of workers (default: 10)")
+
+    if args.port is None:
+        args.port = 50051 if args.server == 'grpc' else 8000
+    if args.host is None:
+        args.host = '[::]' if args.server == 'grpc' else '0.0.0.0'
 
     return parser.parse_args()
 
 if __name__ == '__main__':
     args = parse_arguments()
-    RAG = RAGPretrainedModel.from_index(f'{RAGATOUILLE_PATH}/{args.index_name}')
-    serve(RAG, args.port, args.max_workers)
+    RAG = RAGPretrainedModel.from_index(f'{RAGATOUILLE_PATH}/{args.index}')
+    logging.info(f"Loaded index from {RAGATOUILLE_PATH}/{args.index}")
+    if args.server == 'grpc':
+        grpc_server.serve(RAG, args.port, args.max_workers)
+    elif args.server == 'fastapi':
+        fastapi_server.serve(RAG, args.host, args.port)
