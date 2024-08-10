bde62a4595a1678a7bdfb3f4ec144a5931cb5393
Ricardo La Rosa <ricardo@larosa.dev>
2024-08-10 11:36:25 +0000

    gitrepo def now uses pygments

--- src/gitrepo.py
+++ src/gitrepo.py
@@ -4,8 +4,9 @@ from typing import Any,List, Tuple, Dict, Set
 from git import Repo, exc
 import time
 import hashlib
-import mimetypes
 from collections import defaultdict
+from pygments.lexers import guess_lexer_for_filename
+from pygments.util import ClassNotFound
 
 class RepoCloneError(Exception):
     """Exception raised for errors in the repository cloning process."""
@@ -18,7 +19,10 @@ class FileProcessingError(Exception):
 def get_repo(
         repo_name: str, 
         ext_blacklist: Set[str] = set(), 
-        dir_blacklist: Set[str] = set()) -> Dict[str, Tuple[List[str], List[Dict[str, Any]], List[str]]]:
+        dir_blacklist: Set[str] = set(),
+        sample_ratio: float = 10,
+        sample_min: int = 512,
+        sample_max: int = 2048) -> Dict[str, Tuple[List[str], List[Dict[str, Any]], List[str]]]:
     """
     Clone a GitHub repository and read its contents, excluding files with blacklisted extensions.
 
@@ -26,6 +30,9 @@ def get_repo(
     repo_name (str): The name of the GitHub repository in the format "username/repo-name".
     ext_blacklist (Set[str]): A set of file extensions to exclude (e.g., {'.exe', '.dll'}).
     dir_blacklist (Set[str]): A set of directory names to exclude (e.g., {'node_modules', 'build'}).
+    sample_ratio (float): Percentage of each file to read for language detection (default 10%).
+    sample_min (int): Minimum number of bytes to read from each file (default 100).
+    sample_min (int): Maximum number of bytes to read from each file (default 1000).
 
     Returns:
     Dict[str, Tuple[List[str], List[Dict[str, Any]], List[str]]]: A dictionary where each key is the filetype and the value is a tuple of (file_contents, metadata, ids).
@@ -49,7 +56,6 @@ def get_repo(
                 file_path = os.path.join(root, file)
                 relative_path = os.path.relpath(file_path, temp_dir)
                 _, file_extension = os.path.splitext(file)
-
                 # Skip files with blacklisted extensions
                 if file_extension.lower() in ext_blacklist:
                     continue
@@ -57,35 +63,32 @@ def get_repo(
                 try:
                     with open(file_path, 'rb') as f:
                         content = f.read()
+                    document = content.decode('utf-8', errors='replace') 
 
                     file_stat = os.stat(file_path)
-                    file_meta = {
-                        "filename": file,
-                        "path": relative_path,
-                        "size_bytes": file_stat.st_size,
-                        # "last_modified": time.ctime(file_stat.st_mtime),
-                        # "created_time": time.ctime(file_stat.st_ctime),
-                        # "file_mode": stat.filemode(file_stat.st_mode),
-                        # "is_symlink": os.path.islink(file_path),
-                        # "mime_type": get_mime_type(file_path),
-                        # determine what programming language the file is written in, based on mime type
-                        "type": get_mime_type(file_path).split('/')[1] if get_mime_type(file_path).startswith("text/") else "unknown",
-                        "md5_hash": hashlib.md5(content).hexdigest(),
-                        "extension": file_extension,
-                        # "is_executable": bool(file_stat.st_mode & stat.S_IXUSR),
-                    }
+                    # Calculate how many characters to use for language detection
+                    sample_size = max(min(int(len(document) * sample_percentage / 100), max_chars), min_chars)
+                    sample = document[:sample_size]
+                    lexer = guess_lexer_for_filename(file_path, sample)
+                    language = lexer.name
 
-                    filetype = file_meta["type"]
-                    # Append data to the corresponding lists in collections
-                    collections[filetype][0].append(content.decode('utf-8', errors='replace'))
-                    collections[filetype][1].append(relative_path)
-                    collections[filetype][2].append(file_meta)
+                except ClassNotFound:
+                    language = "Unknown"
 
                 except Exception as e:
                     raise FileProcessingError(f"Error processing file {file_path}: {str(e)}")
+                
+                file_meta = {
+                    "filename": file,
+                    "path": relative_path,
+                    "size_bytes": file_stat.st_size,
+                    "language": language,
+                    "md5_hash": hashlib.md5(content).hexdigest(),
+                    "extension": file_extension,
+                }
+
+                collections[language][0].append(document) # collection
+                collections[language][1].append(relative_path) # document_id
+                collections[language][2].append(file_meta) # document _metadata
 
         return dict(collections) 
-
-def get_mime_type(file_path: str) -> str:
-    mime_type, _ = mimetypes.guess_type(file_path)
-    return mime_type or "application/octet-stream"
